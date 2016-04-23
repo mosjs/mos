6334958b0138967f695f98884de45a96572653ae
Zoltan Kochan <zoltan.kochan@gmail.com>
2016-04-24 00:40:51 +0300

    fix(plugin-dependencies): do not fail when no dependencies in the package.json

--- plugins/mos-plugin-dependencies/lib/get-deps.js
+++ plugins/mos-plugin-dependencies/lib/get-deps.js
@@ -5,7 +5,7 @@ const gh = require('github-url-to-object')
 const path = require('path')
 
 function getDeps (opts) {
-  return Object.keys(opts.deps).map(depname => {
+  return Object.keys(opts.deps || {}).map(depname => {
     const reqPath = `${opts.pkgRoot}/node_modules/${depname}/package.json`
     const dep = require(path.resolve(reqPath))
     if (dep.repository && dep.repository.url && gh(dep.repository.url)) {
