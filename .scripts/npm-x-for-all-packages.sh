eb26b19bf208d3af26702ac46ee595b4bb27fd99
Zoltan Kochan <zoltan.kochan@gmail.com>
2016-09-18 12:13:09 +0300

    chore: use pnpm with link-local

---
+++ .scripts/npm-x-for-all-packages.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+exitstatus=0
+
+for d in packages/*; do
+  echo "> ($d)";
+  echo "> pnpm $@";
+  echo "";
+  cd $d;
+  npm $@ || exitstatus=$?;
+  cd ..;
+  if [ $exitstatus -ne 0 ]; then
+    break;
+    exit $exitstatus;
+  fi
+done
+
+exit $exitstatus
